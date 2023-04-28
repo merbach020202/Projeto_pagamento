fa4b73309b999d9ccf3a1bbc20bf0757d7c8eebc
Luiz <38606125814@info.local>
2023-04-28 10:29:20 -0300

    Fiz classe debito

---
+++ Projeto pagamento-menu/ClassDebito/Debito.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_pagamento.ClassDebito
+{
+    public class Debito
+    {
+        private float Saldo;
+    }
+}
