06a33763ca02a58a61679f68a310489e9efd7ff2
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-05-03 08:06:39 -0300

    checkout lucas

--- Boleto.cs
+++ Boleto.cs
@@ -7,7 +7,7 @@ namespace Projeto_pagamento
 {
     public class Boleto : Pagamento
     {
-         public void PagamentoBoleto(double Valor)
+        public void PagamentoBoleto(double Valor, DateTime dataVencimento)
         {
             double valorBoleto = Valor * 0.88;
             string codigoBarras = GerarCodigoBarras();
@@ -15,8 +15,12 @@ namespace Projeto_pagamento
             Console.WriteLine(titular);
             Console.WriteLine("Valor a ser pago: R$ " + valorBoleto.ToString("F2"));
             Console.WriteLine("Código de barras: " + codigoBarras);
-        }
+            Console.WriteLine("Data de vencimento: " + dataVencimento.ToString("dd/MM/yyyy"));
 
+            // Adiciona 3 dias à data de vencimento
+            DateTime dataLimite = dataVencimento.AddDays(3);
+            Console.WriteLine("Data limite para pagamento: " + dataLimite.ToString("dd/MM/yyyy"));
+        }
         public string GerarCodigoBarras()
         {
             Random rnd = new Random();
@@ -29,6 +33,12 @@ namespace Projeto_pagamento
 
             return codigoBarras;
         }
+
+
+
+
+
+
 
         //  public string GerarCodigoBarras()
         // {
